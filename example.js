ff6ff810acb64cf36439898d64dcc1ca0c860a86
Brian Stauffer <brianrstauffer@gmail.com>
2013-02-24 01:04:35 -0500

    Added retrieval of shown/hidden elements, more documentation

--- example.js
+++ example.js
@@ -1,5 +1,5 @@
 $(document).ready( function() {
-  var filter_list = new flist( $("#flist") );
+  var filter_list = new FList( $("#flist") );
   
   $("input").change( function () {
     filter_list.filter( $(this).val() );
--- flist.js
+++ flist.js
@@ -3,14 +3,15 @@
  */
  
 /**
- * Initializes an FList filter list on an unordered list DOM element.
  * @constructor
+ * Initializes an FList filter list on an unordered list DOM element.
  * @requires jQuery
  * @param list (jQuery) A jQuery DOM <ul> or <ol> to create an FList around
  */
 function FList( list ) {
   this.list = list;
   this.mtime = 5;
+  this.list.css({"overflow" : "hidden"});
   
   // Wrap each list element's contents with a div to use to restore height
   $(list.find("li")).wrapInner('<div />').addClass("showing");
@@ -24,6 +25,7 @@ jQuery.expr[':'].Contains = function(a,i,m){
 };
 
 /**
+ * @method filter
  * Filter the filter list by a given filter string, with animations.
  * @param filter_string String to filter the list by
  */
@@ -46,42 +48,45 @@ FList.prototype.filter = function( filter_string ) {
 }
 
 /**
- * Create a function to hide an element.
- * @param list_element jQuery list element to hide
- * @return (function) A function that hides the list element when called
+ * @method getVisibleElements
+ * Retrieves a jQuery list of all visible elements depending on the filter.
+ * @return (jQuery) List of visible elements in the list
  */
-FList.prototype.hideElement = function( list_element ) {
-  if( !list_element.hasClass("hide") )
-    return function() {};
-  list_element.removeClass("hide").addClass("hiding");
-  list_element.stop();
-  var flist_this = this;
-  
-  return function() {
-    // If this element has been "hidden" before this callback was reached, stop
-    flist_this.hide( list_element );
-  }
+FList.prototype.getVisibleElements = function() {
+  return this.list.find(".show, .showing");
 }
 
 /**
- * Create a function to show an element.
- * @param list_element jQuery list element to show
- * @return (function) A function that shows the list element when called
+ * @method getInvisibleElements
+ * Retrieves a jQuery list of all invisible/hidden elements depending on the filter.
+ * @return (jQuery) List of invisible/hidden elements in the list
  */
-FList.prototype.showElement = function( list_element ) {
-  if( !list_element.hasClass("show") )
-    return function() {};
-  list_element.removeClass("show").addClass("showing");
-  list_element.stop();
-  var flist_this = this;
-  
-  return function() {
-    // If this element has been "shown" before this callback was reached, stop
-    flist_this.show( list_element );
-  }
+FList.prototype.getInisibleElements = function() {
+  return this.list.find(".hide, .hidden");
 }
 
 /**
+ * @method hasShowing
+ * Function to check if an element has one of the "showing" classes.
+ * @param element (jQuery) List element to check the classes of
+ * @return (Boolean) True if element has the class "show" or "showing", false otherwise
+ */
+FList.prototype.hasShowing = function( element ) {
+  return element.hasClass("show") || element.hasClass("showing");
+}
+
+/**
+ * @method hasHiding
+ * Function to check if an element has one of the "hiding" classes.
+ * @param element (jQuery) List element to check the classes of
+ * @return (Boolean) True if element has the class "hide" or "hiding", false otherwise
+ */
+FList.prototype.hasHiding = function( element ) {
+  return element.hasClass("hide") || element.hasClass("hiding");
+}
+
+/**
+ * @method hide
  * Hides a given jQuery element of the filter list.  Change this function to 
  * change animation behavior.  The replacing function must accept one parameter,
  * which is the jQuery list element to hide.
@@ -100,6 +105,26 @@ FList.prototype.hide = function( element ) {
 }
 
 /**
+ * @method hideElement
+ * Create a function to hide an element.
+ * @param list_element jQuery list element to hide
+ * @return (function) A function that hides the list element when called
+ */
+FList.prototype.hideElement = function( list_element ) {
+  if( !list_element.hasClass("hide") )
+    return function() {};
+  list_element.removeClass("hide").addClass("hiding");
+  list_element.stop();
+  var flist_this = this;
+  
+  return function() {
+    // If this element has been "hidden" before this callback was reached, stop
+    flist_this.hide( list_element );
+  }
+}
+
+/**
+ * @method show
  * Shows a given jQuery element of the filter list.  Change this function to 
  * change animation behavior.  The replacing function must accept one parameter,
  * which is the jQuery list element to show.
@@ -119,19 +144,20 @@ FList.prototype.show = function( element ) {
 }
 
 /**
- * Function to check if an element has one of the "showing" classes.
- * @param element (jQuery) List element to check the classes of
- * @return (Boolean) True if element has the class "show" or "showing", false otherwise
+ * @method showElement
+ * Create a function to show an element.
+ * @param list_element jQuery list element to show
+ * @return (function) A function that shows the list element when called
  */
-FList.prototype.hasShowing = function( element ) {
-  return element.hasClass("show") || element.hasClass("showing");
-}
-
-/**
- * Function to check if an element has one of the "hiding" classes.
- * @param element (jQuery) List element to check the classes of
- * @return (Boolean) True if element has the class "hide" or "hiding", false otherwise
- */
-FList.prototype.hasHiding = function( element ) {
-  return element.hasClass("hide") || element.hasClass("hiding");
+FList.prototype.showElement = function( list_element ) {
+  if( !list_element.hasClass("show") )
+    return function() {};
+  list_element.removeClass("show").addClass("showing");
+  list_element.stop();
+  var flist_this = this;
+  
+  return function() {
+    // If this element has been "shown" before this callback was reached, stop
+    flist_this.show( list_element );
+  }
 }
